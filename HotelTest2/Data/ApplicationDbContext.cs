93004492f5e5686ad2234ca29f45770b41c461fc
cedaomirLjepojevic <cedomir.ljepojevic@sistemi.rs>
2023-08-28 11:11:21 +0200

    first commit

---
+++ HotelTest2/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HotelTest2.Models;
+
+namespace HotelTest2.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    DbSet<Hotel> hotels { get; set; }
+}
+
---
+++ HotelTest2/Models/Hotel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelTest2.Models
+{
+	public class Hotel
+	{
+		public int HotelID { get; set; }
+		public string RoomNumber { get; set; }
+		public string RoomImage { get; set; }
+		public int RoomPrice { get; set; }
+		public int BookingStatusID { get; set; }
+		public int RoomTypeID { get; set; }
+		public int RoomCapacity { get; set; }
+		public string RoomDescription { get; set; }
+	}
+}
+
