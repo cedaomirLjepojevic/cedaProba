1bee2ada65d242ad63a4cc05accbdad6e5ab1a06
cedaomirLjepojevic <cedomir.ljepojevic@sistemi.rs>
2023-08-28 11:21:29 +0200

    proba na mojoj grani

--- HotelTest2/Controllers/RoomController.cs
+++ HotelTest2/Controllers/RoomController.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+//Ovo je samo test
+
 namespace HotelTest2.Controllers
 {
     public class RoomController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
